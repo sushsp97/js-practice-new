7bf741ca641d6332c0d0f44b2742da130ae35be6
Sushreeta Pradhan <48162275+sushsp97@users.noreply.github.com>
2024-08-21 08:25:13 +0000

    conversion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,8 @@
+let score = "33abc"
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber); // NaN
+
+// ********************** Operations *************************
+
+
