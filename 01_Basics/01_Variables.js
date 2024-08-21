d55d226ebe5d1fb68235d75d5c8a9738c575ce1f
Sushreeta Pradhan <48162275+sushsp97@users.noreply.github.com>
2024-08-21 05:35:45 +0000

    dataTypes

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,13 @@
+const accountId = 11445
+let accountEmail = "sushreeta@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bhubaneswar"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "sush@gmail.com"
+accountPassword = "123"
+accountCity = "BBSR"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,5 @@
+"use strict";  // Treats all js code as newer version
+// alert(3+3)  // we are using nodejs not browser
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
+
