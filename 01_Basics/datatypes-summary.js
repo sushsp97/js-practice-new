1628c9a53d6eab3105f3e25574e5c05ac229cdc6
Sushreeta Pradhan <48162275+sushsp97@users.noreply.github.com>
2024-08-21 11:40:48 +0000

    datatypes

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,20 @@
+// Primitive : String, Number, BigInt, Null, Undefined, Boolean, Symbol  // Call for value
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId); // false
+
+const bigNumber = 228837636552543n
+
+// Reference (Non Primitive) : Array, Object, Functions
+
+const heroes = ["shaktiman", "naagraj"]
+let myObj = 
+{
+    name : "sush",
+    age : 18
+}
+
+const myFunction = function(){
+    console.log("Hello World !"); 
+}
